54a9476572409c84f399ececf0dae566503ee7b9
WahyuGiri04 <88410452+WahyuGiri04@users.noreply.github.com>
2021-11-24 11:36:26 +0700

    client

---
+++ Reimbursement/Client/Repository/Data/LoginRepository.cs
@@ -0,0 +1,42 @@
+﻿using API.Viewmodels;
+using API.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Base.Url;
+using Client.Repository;
+
+namespace Client.Repositories.Data
+{
+    public class LoginRepository : GeneralRepository<LoginVM, string>
+    {
+        private readonly Address address;
+        private readonly HttpClient httpClient;
+        private readonly string request;
+        public LoginRepository(Address address, string request = "Employees/") : base(address, request)
+        {
+            this.address = address;
+            this.request = request;
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address.link)
+            };
+        }
+        public async Task<JWTokenVM> Auth(LoginVM login)
+        {
+            JWTokenVM token = null;
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+            var result = await httpClient.PostAsync(request + "Sign", content);
+
+            string apiResponse = await result.Content.ReadAsStringAsync();
+            token = JsonConvert.DeserializeObject<JWTokenVM>(apiResponse);
+
+            return token;
+        }
+    }
+}
