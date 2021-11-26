610738ec9958aab81257d10d5ec1c69c9f61b422
WahyuGiri04 <88410452+WahyuGiri04@users.noreply.github.com>
2021-11-26 08:26:00 +0700

    b

--- Reimbursement/Client/Controllers/RolesController.cs
+++ Reimbursement/Client/Controllers/RolesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Models;
+using Client.Base.Controllers;
+using Client.Repository.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +9,13 @@ using System.Threading.Tasks;
 
 namespace Client.Controllers
 {
-    public class RolesController : Controller
+    public class RolesController : BaseController<Role, RoleRepository, int>
     {
+        private readonly RoleRepository repository;
+        public RolesController(RoleRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
         public IActionResult Index()
         {
             return View();
--- Reimbursement/Client/Repository/Data/RoleRepository.cs
+++ Reimbursement/Client/Repository/Data/RoleRepository.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using API.Models;
+using Client.Base.Url;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Client.Repository.Data
 {
-    public class RoleRepository
+    public class RoleRepository: GeneralRepository<Role, int>
     {
+        private readonly Address address;
+        private readonly HttpClient httpClient;
+        private readonly string request;
+        public RoleRepository(Address address, string request = "Roles/") : base(address, request)
+        {
+            this.address = address;
+            this.request = request;
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address.link)
+            };
+        }
     }
 }
