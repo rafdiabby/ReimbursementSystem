134ae1b7cf6dad92474a17f8d2d5e1ac1fd3fa0d
WahyuGiri04 <88410452+WahyuGiri04@users.noreply.github.com>
2021-11-29 12:06:31 +0700

    add-profile menu

---
+++ Reimbursement/Client/wwwroot/Js/Profile.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    var nik = document.getElementById("sessionNIK").value;
+    console.log(nik);
+
+    $.ajax({
+        url: "/employees/get/" + nik,
+        success: function (result) {
+
+            var nik = result.nik;
+            var fullName = result.firstName + ' ' + result.lastName;
+            var email = result.email;
+            var phone = '+62'+ result.phone;
+            var gender = "";
+            if (result.gender == 0) {
+                gender = "Male";
+            } else {
+                gender = "Female";
+            }
+            var noRekening = result.bankAccount;
+            console.log(fullName);
+            document.getElementById("nik").innerHTML = nik;
+            document.getElementById("fullname").innerHTML = fullName;
+            document.getElementById("email").innerHTML = email;
+            document.getElementById("phone").innerHTML = phone;
+            document.getElementById("gender").innerHTML = gender;
+            document.getElementById("noRekening").innerHTML = noRekening;
+        }
+    });
+
+    $.ajax({
+        url: "/Employees/GetRole/" + nik,
+        success: function (hasil) {
+            var dataRole = [];
+            $.each(hasil, function (key, val) {
+                dataRole.push(val.roleName);
+            });
+            var acRole = dataRole.toString();
+            document.getElementById("acRole").innerHTML = acRole;
+        }
+    });
+})
---
+++ Reimbursement/Client/wwwroot/Js/Reset.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    var nik = document.getElementById("sessionNIK").value;
+    console.log(nik);
+
+    document.getElementById("id").value = nik;
+});
